40760f74b4dfeca3411c9e018ba16a3a30697726
Joseph Morain <morain@echoworx.com>
2018-05-17 17:21:19 -0400

    add log4j2

--- src/main/java/com/cruat/tools/projection/Program.java
+++ src/main/java/com/cruat/tools/projection/Program.java
@@ -1,7 +1,11 @@
 package com.cruat.tools.projection;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Program {
+	public static final Logger logger = LogManager.getLogger();
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		logger.debug("Hello World!");
 	}
 }
