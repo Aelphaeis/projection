45d5d304a5fd99a9452953d5756702c7b6ecde50
Joseph Morain <morain@echoworx.com>
2018-05-17 17:33:09 -0400

    added base concepts for projection

---
+++ src/main/java/com/cruat/tools/projection/core/ConflictResolution.java
@@ -0,0 +1,8 @@
+package com.cruat.tools.projection.core;
+
+public enum ConflictResolution {
+	OVERWRITE,
+	MODIFY_SOURCE,
+	MODIFY_TARGET,
+	SKIP
+}
---
+++ src/main/java/com/cruat/tools/projection/core/Projector.java
@@ -0,0 +1,34 @@
+package com.cruat.tools.projection.core;
+
+/**
+ * This class represents an entity that is responsible for dictating the 
+ * behavior surrounding how something should project a resource from
+ * one location to another.
+ * 
+ * @author morain
+ *
+ */
+public interface Projector<T> {
+	/**
+	 * Represents a singular operation of projection
+	 * @return
+	 */
+	boolean project();
+	/**
+	 * Get the source of the projection.
+	 * @return
+	 */
+	T getSource();
+	/**
+	 * Get the Target of the projection.
+	 * @return
+	 */
+	T getTarget();
+	
+	/**
+	 * During projection if there is a conflict where their already exists
+	 * a resource how should the projection handle it.
+	 * @return
+	 */
+	ConflictResolution getConflictResolutionStrategy();
+}
