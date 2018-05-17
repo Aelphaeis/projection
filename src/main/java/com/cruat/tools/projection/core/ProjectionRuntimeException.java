1b03868298a88ff7b1e75b18a73586c2f3fb7923
Joseph Morain <morain@echoworx.com>
2018-05-17 17:44:57 -0400

    added exceptions

---
+++ src/main/java/com/cruat/tools/projection/core/ProjectionRuntimeException.java
@@ -0,0 +1,20 @@
+package com.cruat.tools.projection.core;
+
+public class ProjectionRuntimeException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ProjectionRuntimeException() {
+		super();
+	}
+	public ProjectionRuntimeException(String message) {
+		super(message);
+	}
+
+	public ProjectionRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ProjectionRuntimeException(Throwable cause) {
+		super(cause);
+	}
+}
