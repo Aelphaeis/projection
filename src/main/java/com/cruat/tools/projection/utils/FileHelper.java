d77b998a935c068c75c35087fd505bdc6fe5091c
Joseph Morain <morain@echoworx.com>
2018-05-18 10:57:48 -0400

    delete contents in fileHelper

--- src/main/java/com/cruat/tools/projection/utils/FileHelper.java
+++ src/main/java/com/cruat/tools/projection/utils/FileHelper.java
@@ -1,6 +1,8 @@
 package com.cruat.tools.projection.utils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,19 +20,44 @@ public class FileHelper {
 		File[] children = dir.listFiles();
 		if (children == null) {
 			fileTree.add(dir);
-		} 
-		else {
+		} else {
 			for (File entry : children) {
 				if (entry.isFile())
 					fileTree.add(entry);
-				else if(recursive)
+				else if (recursive)
 					fileTree.addAll(listFileTree(entry, recursive));
 			}
 		}
 		return fileTree;
 	}
-	
+
+	/**
+	 * Deletes the contents of a folder without deleting the folder.
+	 * 
+	 * @param folder
+	 * @return true if anything was deleted, otherwise false
+	 */
+	public static boolean deleteContents(File folder) {
+		File[] files = folder.listFiles();
+		boolean deleted = false;
+		if(files == null) {
+			return deleted;
+		}
+		try {
+			for (File f : files) {
+				if (f.isDirectory()) {
+					deleted |= deleteContents(f);
+				} else {
+					deleted |= Files.deleteIfExists(f.toPath());
+				}
+			}
+		}
+		catch(IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+		return deleted;
+	}
 	private FileHelper() {
-		//utility class
+		// utility class
 	}
 }
