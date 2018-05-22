9dd1aa69dd6a203d43e8481e19a514e101493927
Joseph Morain <morain@echoworx.com>
2018-05-22 12:09:13 -0400

    implemented file projector

--- src/main/java/com/cruat/tools/projection/core/FileProjector.java
+++ src/main/java/com/cruat/tools/projection/core/FileProjector.java
@@ -1,9 +1,14 @@
 package com.cruat.tools.projection.core;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class FileProjector implements Projector<File> {
 
+	private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
 	private final File source;
 	private final File target;
 	ConflictResolution resolutionStrategy;
@@ -24,20 +29,69 @@ public class FileProjector implements Projector<File> {
 	}
 
 	private void validateSource() {
-		if(!getSource().exists()) {
+		if (!getSource().exists()) {
 			String err = "source must exist";
 			throw new IllegalArgumentException(err);
 		}
-		if(!getSource().isFile()) {
+		if (!getSource().isFile()) {
 			String err = "source must be a file";
 			throw new IllegalArgumentException(err);
 		}
 	}
-	
 
 	@Override
-	public boolean project() {
+	public boolean project() throws ProjectionException {
+		validateSource();
+		// check source exists
+
+		File t = getTarget();
+		// does targets directory exist ?
+		if (t.getParentFile() != null && !t.getParentFile().exists()) {
+			t.getParentFile().mkdirs();
+		}
+
+		// make sure we can write to target
+		if (t.exists() && !t.canWrite()) {
+			String err = "Unable to open file " + target + " for writing.";
+			throw new ProjectionException(err);
+		}
+
+		// makes sure it is not the same file
+		try {
+			if (source.getCanonicalPath().equals(t.getCanonicalPath())) {
+				String err = "Unable to write file " + source + " on itself.";
+				throw new ProjectionException(err);
+			}
+		} catch (IOException e) {
+			throw new ProjectionException(e);
+		}
+
+		try {
+			copy(source, t);
+		} catch (IOException e) {
+			throw new ProjectionException(e);
+		}
+
+		if (source.length() != t.length()) {
+			String err = "Failed to copy full contents from  source to target";
+			throw new ProjectionException(err);
+		}
 		return false;
+	}
+
+	static void copy(File s, File t) throws IOException {
+		FileInputStream i = null;
+		try (OutputStream o = new FileOutputStream(t)) {
+			i = new FileInputStream(s);
+			byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+			for (int n = 0; (n = i.read(buffer)) != -1;) {
+				o.write(buffer, 0, n);
+			}
+		} finally {
+			if (i != null) {
+				i.close();
+			}
+		}
 	}
 
 	@Override
