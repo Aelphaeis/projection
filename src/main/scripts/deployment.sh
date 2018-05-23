544637a96db4eeb62b7cf96d1bd9f34819a03224
Joseph Morain <morain@echoworx.com>
2018-05-23 13:40:23 -0400

    fix bug with env variable names.

--- src/main/scripts/deployment.sh
+++ src/main/scripts/deployment.sh
@@ -1,4 +1,4 @@
 cp .travis.settings.xml $HOME/.m2/settings.xml
-sed -i 's/${env.prefix}/'$username'/g' $HOME/.m2/settings.xml
-sed -i 's/${env.prefix}/'$password'/g' $HOME/.m2/settings.xml
+sed -i 's/${env.username}/'$username'/g' $HOME/.m2/settings.xml
+sed -i 's/${env.password}/'$password'/g' $HOME/.m2/settings.xml
 mvn deploy
