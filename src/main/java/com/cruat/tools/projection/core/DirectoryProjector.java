dcb880c5afbeec38446043af8d98d504efc00857
Joseph Morain <morain@echoworx.com>
2018-05-23 16:21:22 -0400

    added directory projector.

---
+++ src/main/java/com/cruat/tools/projection/core/DirectoryProjector.java
@@ -0,0 +1,31 @@
+package com.cruat.tools.projection.core;
+
+import java.io.File;
+
+public class DirectoryProjector implements Projector<File> {
+
+	@Override
+	public boolean project() throws ProjectionException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public File getSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File getTarget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ConflictResolution getConflictResolutionStrategy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
