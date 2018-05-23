a4464c043f0f2151db5d9d9357733c09dbe6a397
Joseph Morain <morain@echoworx.com>
2018-05-23 13:38:21 -0400

    added stream editing for environment variables in deployment

--- src/main/scripts/deployment.sh
+++ src/main/scripts/deployment.sh
@@ -1,2 +1,4 @@
 cp .travis.settings.xml $HOME/.m2/settings.xml
+sed -i 's/${env.prefix}/'$username'/g' $HOME/.m2/settings.xml
+sed -i 's/${env.prefix}/'$password'/g' $HOME/.m2/settings.xml
 mvn deploy
