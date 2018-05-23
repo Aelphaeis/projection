dfc423d2bc7aeee5066146c1de3cf50dd0790497
Joseph <aelphaeis@live.com>
2018-05-23 19:37:46 -0400

    update deployment script

--- src/main/scripts/deployment.sh
+++ src/main/scripts/deployment.sh
@@ -1,4 +1,4 @@
+sed -i 's/${env.username}/'$username'/g' .travis.settings.xml
+sed -i 's/${env.password}/'$password'/g' .travis.settings.xml
 cp .travis.settings.xml $HOME/.m2/settings.xml
-sed -i 's/${env.username}/'$username'/g' $HOME/.m2/settings.xml
-sed -i 's/${env.password}/'$password'/g' $HOME/.m2/settings.xml
 mvn deploy
